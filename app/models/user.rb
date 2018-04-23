b64c2a5bdd500c19b95f7e1f48156ac5eaed3309
Vterebenin <niceserver11@mail.ru>
2018-04-23 18:44:36 +0300

    Add password resets

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,7 @@ class User < ApplicationRecord
     update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
+  # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
